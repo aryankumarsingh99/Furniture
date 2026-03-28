58dc893a8fa44ba8744b69b733cc786c56ffabc0
aryankumarsingh <aryankumarsingh8340@gmail.com>
2026-03-28 19:32:00 +0530

    add contact page component

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+
+export default function ContactPage() {
+	return (
+		<div>
+			{/* Contact page content goes here */}
+			<h1>Contact Us</h1>
+			<p>This is the contact page.</p>
+		</div>
+	);
+}
