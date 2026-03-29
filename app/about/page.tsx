af6a6f91da7f0c797b0a3007781c86dca5c4cba7
aryankumarsingh <aryankumarsingh8340@gmail.com>
2026-03-29 12:16:04 +0530

    Refactor AboutPage component: clean up unused section imports and comments for improved readability

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,9 +4,10 @@ import Navbar from "../../components/Navbar";
 import { useState } from "react";
 import AboutHero from "../../components/About/hero";
 import AboutSection from "../../components/About/AboutSection";
-import OurStorySection from "../../components/About/OurStorySection";
-import OurTeamSection from "../../components/About/OurTeamSection";
-import SustainabilitySection from "../../components/About/SustainabilitySection";
+import Footer from "../../components/Footer";
+// import OurStorySection from "../../components/About/OurStorySection";
+// import OurTeamSection from "../../components/About/OurTeamSection";
+// import SustainabilitySection from "../../components/About/SustainabilitySection";
 
 export default function AboutPage() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -16,9 +17,10 @@ export default function AboutPage() {
       <div className={menuOpen ? "transition-all duration-300 filter blur-sm pointer-events-none select-none" : "transition-all duration-300"}>
         <AboutHero />
         <AboutSection />
-        <OurStorySection />
+        {/* <OurStorySection />
         <OurTeamSection />
-        <SustainabilitySection />
+        <SustainabilitySection /> */}
+        <Footer />
       </div>
     </div>
   );
